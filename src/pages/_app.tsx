957485be4144ea88e81c0b644010a4cc0cc4b3ba
MikiS <73443709+RAV64@users.noreply.github.com>
2023-02-25 15:41:07 +0200

    new favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,8 +10,14 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 		<>
 			<Head>
 				<title>Full Stack Library</title>
-				<meta name="description" />
-				<link rel="icon" href="/favicon.ico" />
+				<meta
+					name="description"
+					content="Full stack Library application using T3 stack"
+				/>
+				<link
+					rel="icon"
+					href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📚</text></svg>"
+				/>
 			</Head>
 			<Component {...pageProps} />
 		</>
